be223def637705b4930a7fc49240ee1b0c9fed4e
oneachina <ruiruixi@hotmail.com>
2026-05-09 21:57:37 +0800

    Refactor module paths for improved readability

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,12 @@ use windows::core::w;
 use winit::event_loop::EventLoop;
 
 fn main() {
-    let config = crate::core::persistence::load_config();
+    let config = core::persistence::load_config();
     init_i18n(&config.language);
 
     let args: Vec<String> = env::args().collect();
     if args.iter().any(|arg| arg == "--settings") {
-        crate::window::settings::run_settings(config);
+        window::settings::run_settings(config);
     } else {
         unsafe {
             let _ = CreateMutexW(None, true, w!("Local\\WinIsland_SingleInstance_Mutex"));
@@ -31,7 +31,7 @@ fn main() {
         let runtime = tokio::runtime::Runtime::new().unwrap();
         let _guard = runtime.enter();
 
-        crate::utils::updater::start_update_checker();
+        utils::updater::start_update_checker();
 
         let event_loop = EventLoop::new().unwrap();
         let mut app = App::default();
