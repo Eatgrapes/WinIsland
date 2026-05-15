83ae285f2d8635d7afef591e98265fd514bb7217
oneachina <ruiruixi@hotmail.com>
2026-05-15 21:12:06 +0800

    refactor(config): remove unnecessary blank line in DockPosition implementation
    Signed-off-by: oneachina <ruiruixi@hotmail.com>

--- src/core/config.rs
+++ src/core/config.rs
@@ -47,7 +47,6 @@ impl DockPosition {
     }
 }
 
-
 impl std::fmt::Display for DockPosition {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(self.as_str())
