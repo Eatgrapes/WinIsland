451a26b9f6594b739cda8af19411833d4ae78518
Eatgrapes <Eatgrapesem232@qq.com>
2026-07-14 12:25:49 +0800

    feat(settings): improve sidebar icons

--- src/window/settings/sidebar.rs
+++ src/window/settings/sidebar.rs
@@ -1,10 +1,44 @@
+use std::cell::OnceCell;
+
 use crate::core::i18n::tr;
 use crate::utils::color::SettingsTheme;
 use crate::utils::font::{DrawTextCachedParams, FontManager};
 use crate::utils::settings_ui::items::*;
-use skia_safe::{Canvas, Color, Paint, Rect};
+use skia_safe::{Canvas, Color, Data, FilterMode, Image, MipmapMode, Paint, Rect, SamplingOptions};
 
 use super::{SIDEBAR_KEY_BASE, SIDEBAR_ROW_H, SIDEBAR_W, SettingsApp};
+
+const SIDEBAR_ICON_BYTES: [&[u8]; 4] = [
+    include_bytes!("../../../resources/in_app/settings/settings.png"),
+    include_bytes!("../../../resources/in_app/settings/music.png"),
+    include_bytes!("../../../resources/in_app/settings/widget.png"),
+    include_bytes!("../../../resources/in_app/settings/about.png"),
+];
+
+thread_local! {
+    static SIDEBAR_ICONS: OnceCell<[Image; 4]> = const { OnceCell::new() };
+}
+
+fn draw_sidebar_icon(canvas: &Canvas, index: usize, rect: Rect) {
+    SIDEBAR_ICONS.with(|cache| {
+        let icons = cache.get_or_init(|| {
+            SIDEBAR_ICON_BYTES.map(|bytes| {
+                let icon = Image::from_encoded(Data::new_copy(bytes))
+                    .expect("Failed to load sidebar icon");
+                icon.with_default_mipmaps().unwrap_or(icon)
+            })
+        });
+        let mut paint = Paint::default();
+        paint.set_anti_alias(true);
+        canvas.draw_image_rect_with_sampling_options(
+            &icons[index],
+            None,
+            rect,
+            SamplingOptions::new(FilterMode::Linear, MipmapMode::Linear),
+            &paint,
+        );
+    });
+}
 
 impl SettingsApp {
     pub(crate) fn draw_sidebar(&self, canvas: &Canvas, theme: &SettingsTheme) {
@@ -115,93 +149,11 @@ impl SettingsApp {
                 paint.set_color(theme.text_sec);
             }
 
-            // Draw macOS-style icon next to text
-            let icon_bg_rect = Rect::from_xywh(row_x + 8.0, row_y + 6.0, 20.0, 20.0);
-            let mut icon_bg_paint = Paint::default();
-            icon_bg_paint.set_anti_alias(true);
-
-            match i {
-                0 => {
-                    icon_bg_paint.set_color(Color::from_rgb(142, 142, 147)); // Gray
-                    canvas.draw_round_rect(icon_bg_rect, 5.0, 5.0, &icon_bg_paint);
-                    crate::icons::settings::draw_settings_icon(
-                        canvas,
-                        row_x + 18.0,
-                        row_y + 16.0,
-                        255,
-                        0.5,
-                        Color::WHITE,
-                    );
-                }
-                1 => {
-                    icon_bg_paint.set_color(Color::from_rgb(252, 60, 68)); // Pink/Red
-                    canvas.draw_round_rect(icon_bg_rect, 5.0, 5.0, &icon_bg_paint);
-                    crate::icons::music::draw_music_icon(
-                        canvas,
-                        row_x + 18.0,
-                        row_y + 16.0,
-                        255,
-                        0.5,
-                        Color::WHITE,
-                    );
-                }
-                2 => {
-                    icon_bg_paint.set_color(Color::from_rgb(52, 199, 89)); // Green
-                    canvas.draw_round_rect(icon_bg_rect, 5.0, 5.0, &icon_bg_paint);
-
-                    let mut widget_paint = Paint::default();
-                    widget_paint.set_anti_alias(true);
-                    widget_paint.set_color(Color::WHITE);
-                    // Draw a 2x2 grid representing widgets
-                    canvas.draw_round_rect(
-                        Rect::from_xywh(row_x + 12.0, row_y + 10.0, 5.0, 5.0),
-                        1.0,
-                        1.0,
-                        &widget_paint,
-                    );
-                    canvas.draw_round_rect(
-                        Rect::from_xywh(row_x + 19.0, row_y + 10.0, 5.0, 5.0),
-                        1.0,
-                        1.0,
-                        &widget_paint,
-                    );
-                    canvas.draw_round_rect(
-                        Rect::from_xywh(row_x + 12.0, row_y + 17.0, 5.0, 5.0),
-                        1.0,
-                        1.0,
-                        &widget_paint,
-                    );
-                    canvas.draw_round_rect(
-                        Rect::from_xywh(row_x + 19.0, row_y + 17.0, 5.0, 5.0),
-                        1.0,
-                        1.0,
-                        &widget_paint,
-                    );
-                }
-                3 => {
-                    icon_bg_paint.set_color(Color::from_rgb(0, 122, 255)); // Royal Blue
-                    canvas.draw_round_rect(icon_bg_rect, 5.0, 5.0, &icon_bg_paint);
-
-                    let mut info_paint = Paint::default();
-                    info_paint.set_anti_alias(true);
-                    info_paint.set_color(Color::WHITE);
-                    info_paint.set_stroke_width(1.2);
-
-                    info_paint.set_style(skia_safe::paint::Style::Stroke);
-                    canvas.draw_circle((row_x + 18.0, row_y + 16.0), 6.5, &info_paint);
-
-                    info_paint.set_style(skia_safe::paint::Style::Fill);
-                    canvas.draw_circle((row_x + 18.0, row_y + 13.0), 0.8, &info_paint);
-
-                    info_paint.set_style(skia_safe::paint::Style::Stroke);
-                    canvas.draw_line(
-                        (row_x + 18.0, row_y + 15.0),
-                        (row_x + 18.0, row_y + 18.5),
-                        &info_paint,
-                    );
-                }
-                _ => {}
-            }
+            draw_sidebar_icon(
+                canvas,
+                i,
+                Rect::from_xywh(row_x + 8.0, row_y + 6.0, 20.0, 20.0),
+            );
 
             fm.draw_text_cached(DrawTextCachedParams {
                 canvas,
